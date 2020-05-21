7203c5ed3c9508522b8781ac4601c2ed50270959
Boyan Meng <meng.boyan@ufz.de>
2020-05-21 22:19:02 +0200

    assemble mass conservation equations

--- ProcessLib/ThermalTwoPhaseFlowComponential/ThermalTwoPhaseFlowComponentialLocalAssembler.h
+++ ProcessLib/ThermalTwoPhaseFlowComponential/ThermalTwoPhaseFlowComponentialLocalAssembler.h
@@ -100,8 +100,8 @@ struct IntegrationPointData final
     double dxcL_dT;
 
     double integration_weight;
-    NodalMatrixType massOperator;
-    NodalMatrixType diffusionOperator;
+    NodalMatrixType mass_operator;
+    NodalMatrixType diffusion_operator;
 
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
 };
@@ -209,13 +209,13 @@ public:
             _ip_data[ip].integration_weight =
                 sm.integralMeasure * sm.detJ *
                 _integration_method.getWeightedPoint(ip).getWeight();
-            _ip_data[ip].massOperator.setZero(ShapeFunction::NPOINTS,
+            _ip_data[ip].mass_operator.setZero(ShapeFunction::NPOINTS,
                                               ShapeFunction::NPOINTS);
-            _ip_data[ip].diffusionOperator.setZero(ShapeFunction::NPOINTS,
+            _ip_data[ip].diffusion_operator.setZero(ShapeFunction::NPOINTS,
                                                    ShapeFunction::NPOINTS);
-            _ip_data[ip].massOperator.noalias() =
+            _ip_data[ip].mass_operator.noalias() =
                 sm.N.transpose() * sm.N * _ip_data[ip].integration_weight;
-            _ip_data[ip].diffusionOperator.noalias() =
+            _ip_data[ip].diffusion_operator.noalias() =
                 sm.dNdx.transpose() * sm.dNdx * _ip_data[ip].integration_weight;
         }
     }
